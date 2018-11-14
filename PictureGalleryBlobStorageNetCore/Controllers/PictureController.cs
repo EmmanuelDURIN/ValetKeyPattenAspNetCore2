6ca04033019db41371e76fb61844823d34c8eadd
Emmanuel DURIN <e_durin@hotmail.com>
2018-11-14 22:13:35 +0100

    Etape Questions de l'ajout du code de BlobStorageManager

---
+++ PictureGalleryBlobStorageNetCore/Controllers/PictureController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.AspNetCore.Mvc;
+using PictureGalleryBlobStorageNetCore.BlobStorage;
+using PictureGalleryBlobStorageNetCore.Models;
+using PictureGalleryBlobStorageNetCore.ViewModels;
+
+namespace PictureGalleryBlobStorageNetCore.Controllers
+{
+  public class PictureController : Controller
+  {
+    public IActionResult Index()
+    {
+      return View(model: new List<Picture>());
+    }
+    // Action pour voir la page du formulaire
+    public IActionResult Upload()
+    {
+      PictureUploadViewModel p = new PictureUploadViewModel
+      {
+        Date = DateTime.Now,
+      };
+      return View(model: p);
+    }
+    // Action d'envoi d'image
+    [ValidateAntiForgeryToken]
+    [HttpPost]
+    public IActionResult Upload(PictureUploadViewModel pictureViewModel)
+    {
+      if (!ModelState.IsValid)
+        return View();
+      // TODO : utiliser BlobStorageManager pour sauvegarder la liste des images
+      return RedirectToAction(nameof(Index));
+    }
+  }
+}
---
+++ PictureGalleryBlobStorageNetCore/Models/Picture.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace PictureGalleryBlobStorageNetCore.Models
+{
+  public class Picture
+  {
+    // Pour la sauvegarde Cosmos DB
+    [JsonProperty(PropertyName = "id")]
+    public String Id { get; set; }
+    [Required]
+    public String Title { get; set; }
+    [Required]
+    public String ImageUrl { get; set; }
+    [Required]
+    [DisplayFormat(DataFormatString = "{0:D}")]
+    public DateTime Date { get; set; }
+    public String BrowserFile { get; set; }
+  }
+}
