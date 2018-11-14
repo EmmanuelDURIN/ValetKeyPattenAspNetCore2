675f3596c11d64e07055d0df585531c770235d46
Emmanuel DURIN <e_durin@hotmail.com>
2018-11-14 22:13:35 +0100

    Etape Questions de l'ajout du code de BlobStorageManager

---
+++ PictureGalleryBlobStorageNetCore/ViewModels/PictureUploadViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace PictureGalleryBlobStorageNetCore.ViewModels
+{
+  public class PictureUploadViewModel
+  {
+    [Required]
+    public String Title { get; set; }
+    [Required]
+    [DataType(DataType.Date)]
+    public DateTime Date { get; set; }
+    public IFormFile Image { set; get; }
+  }
+}
